b730a6fb4457481183dd11eeefbcd5a72083746d
Robert Allen Weldon Jr <raweldon@wolftech.ad.ncsu.edu>
2018-11-07 15:44:16 -0500

    working 3d scatter plots

--- lo_analysis.py
+++ lo_analysis.py
@@ -56,9 +56,13 @@ def order_by_rot(data, beam_11MeV):
             rot_order = [15, 16, 17, 18, 19, 6, 7, 0, 1, 2, 3, 4, 8, 9, 10, 5, 11, 12, 13, 14]
             angles = np.arange(0, 200, 10)
     else:
-        rot_order = [12, 13, 5, 6, 0, 1, 2, 3, 7, 9, 9, 4, 10, 11]
-        angles =    [0, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180]
-        
+        if len(data.filename) == 15:
+            rot_order = [12, 13, 14, 5, 6, 0, 1, 2, 3, 7, 8, 9, 4, 10, 11]
+            angles =    [0, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 190]
+        else:
+            rot_order = [12, 13, 5, 6, 0, 1, 2, 3, 7, 8, 9, 4, 10, 11]
+            angles =    [0, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180]
+
     data = data.assign(rot_order = rot_order)
     data = data.sort_values('rot_order')
     return data, angles
@@ -92,79 +96,164 @@ def tilt_check(det_data, dets, tilts, beam_11MeV):
             plt.legend()
         plt.show()
 
-def plot_3d(data, theta):
+def plot_3d(data1, data2, det, theta_n, beam_11MeV):
     # convert to cartesian
-    tilt = theta
-    theta = np.deg2rad([90 - t for t in theta])
-    phi = np.deg2rad(data.phi.values)
+    theta1 = data1.theta.values
+    phi1 = data1.phi.values
+    theta2 = data2.theta.values
+    phi2 = data2.phi.values
 
-    colors = cm.jet(np.linspace(0,1,len(tilt)))
-    fig = plt.figure(0)
+    fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    for i, t in enumerate(theta):
-        x = np.sin(t)*np.cos(phi)
-        y = np.sin(t)*np.sin(phi)
-        z = [np.cos(t)]*len(phi)
+    x1 = np.sin(theta1)*np.cos(phi1)
+    y1 = np.sin(theta1)*np.sin(phi1)
+    z1 = np.cos(theta1)
+    x2 = np.sin(theta2)*np.cos(phi2)
+    y2 = np.sin(theta2)*np.sin(phi2)
+    z2 = np.cos(theta2)
 
-        # plot
-        ax.scatter(x, y, z, label=tilt[i], c=colors[i])
-    #ax.set_aspect('equal')
+    # plot
+    ax.scatter(x1, y1, z1, c='r', label='bvert')
+    ax.scatter(x2, y2, z2, c='b', label='cpvert')
+    ax.set_xlim([-1.1,1.1])
+    ax.set_ylim([-1.1,1.1])
+    ax.set_zlim([-1.1,1.1])
+    ax.set_xlabel('a')
+    ax.set_ylabel('b')
+    ax.set_zlabel('c\'')
+    ax.set_aspect('equal')
+    if beam_11MeV:
+        ax.set_title('11.3 MeV beam, det ' + str(det) + '  (' + str(theta_n) +'$^{\circ}$)')
+    else:
+        ax.set_title('4.8 MeV beam, det ' + str(det) + '  (' + str(theta_n) +'$^{\circ}$)')
     plt.tight_layout()
     plt.legend()
-    plt.show()
 
+def crystal_basis_vectors(theta,axis_up):
+    ''' Rotation is counterclockwise about the a-axis (x-axis).
+    '''    
+    theta = np.deg2rad(theta)
+    rot_matix_x = np.asarray(((1,0,0),(0,np.cos(theta),np.sin(theta)),(0,-np.sin(theta),np.cos(theta))))
+    rotated_axes = np.transpose(np.dot(rot_matix_x,np.transpose(axis_up)))  
+    return rotated_axes
 
-def map_3d(data, dets, tilts, beam_11MeV):
-    det_ang_rel_a = [20, 30, 40, 50, 60, 70, 100, 120, 130, 140, 150, 160]
-    for d, det in enumerate(dets):
-        det_df = data.loc[(data.det_no == str(det))]
-        dfs=[]
-        for t, tilt in enumerate(tilts):
-            tilt_df = det_df.loc[(data.tilt == str(tilt))]
-            tilt_df, angles = order_by_rot(tilt_df, beam_11MeV)
+def map_3d(data, det, tilts, crystal_orientation, theta_neutron, phi_neutron, beam_11MeV):
 
-            start = -det_ang_rel_a[d]
-            if beam_11MeV:
-                if len(tilt_df.filename) == 19:
-                    phi = np.arange(start, start + 190, 10)
-                else:
-                    phi = np.arange(start, start + 200, 10)
-            else:
-                phi = [start, start+20, start+30, start+40, start+50, start+60, start+70, start+80, start+90, 
-                       start+100, start+120, start+140, start+160, start+180]
+    det_df = data.loc[(data.det_no == str(det))]
+    dfs=[]
+    for t, tilt in enumerate(tilts):
+        tilt_df = det_df.loc[(data.tilt == str(tilt))]
+        tilt_df, angles = order_by_rot(tilt_df, beam_11MeV)
+
+        basis_vectors = crystal_basis_vectors(tilt, crystal_orientation)
+        thetap, phip = [], []
+        for angle in angles:
+            # code from C:\Users\raweldon\Research\TUNL\crystal_orientation\crystal_orientations_3d_plot_v8.py
+            angle = np.deg2rad(angle)
+            # counterclockwise rotation matrix about y
+            rot_matrix_y = np.asarray(((np.cos(angle), 0, -np.sin(angle)), (0, 1, 0), (np.sin(angle), 0, np.cos(angle))))
+            rot_orientation = np.transpose(np.dot(rot_matrix_y, np.transpose(basis_vectors)))
         
-            proton_phi = [90 - p for p in phi] # theta_p + theta_n = 90deg
-            update_df = tilt_df.assign(phi = proton_phi)
-            dfs.append(update_df)
+            # proton recoil
+            theta_proton = np.deg2rad(90 - theta_neutron) # proton recoils at 90 deg relative to theta
+            phi_proton = np.deg2rad(phi_neutron + 180) # phi_proton will be opposite sign of phi_neutron
+        
+            # cartesian vector    
+            x_p = np.sin(theta_proton)*np.cos(phi_proton)
+            y_p = np.sin(theta_proton)*np.sin(phi_proton)    
+            z_p = np.cos(theta_proton)
+            
+            p_vector = np.asarray((x_p, y_p, z_p))
+            
+            #get theta_p
+            p_vector_dot_cp = np.dot(p_vector,rot_orientation[2]) # a.b=||a||*||b|| cos(theta)
+            theta_p = np.rad2deg(np.arccos(p_vector_dot_cp))
+#            print '  theta_p =',theta_p
+            
+            # get phi_p
+            vector_proj_ab = p_vector - p_vector_dot_cp*rot_orientation[2] # remove c' to get ab plane projection (scalar proj: a1 = a*cos(theta))
+            vector_proj_ab = vector_proj_ab/np.sqrt(np.dot(vector_proj_ab,vector_proj_ab)) # vector proj: a1^ = (|a^|cos(theta)) b^/|b|
+            vector_proj_dot_a = np.dot(vector_proj_ab,rot_orientation[0])
+            
+            # account for rounding errors on 1.0 and -1.0 (ex: 1.00000002 -> phi_p = nan)
+            if abs(vector_proj_dot_a) > 1.0:
+                if vector_proj_dot_a > 0:
+                    vector_proj_dot_a = 1.0
+                else:
+                    vector_proj_dot_a = -1.0
+        
+            phi_p = np.rad2deg(np.arccos(vector_proj_dot_a))
+            
+            # check if phi > 180 deg
+            if np.dot(vector_proj_ab, rot_orientation[1]) < 0:
+                phi_p = 360 - phi_p
 
-        full_df = pd.concat(dfs)
-        plot_3d(full_df, tilts)
-            
-            
+            thetap.append(np.deg2rad(theta_p))
+            phip.append(np.deg2rad(phi_p))                 
+
+        print len(phip), len(thetap)
+        update_df = tilt_df.assign(phi = phip)
+        update_df = update_df.assign(theta = thetap)
+        dfs.append(update_df)
+    return pd.concat(dfs)
 
 
 def main():
     cwd = os.getcwd()
     p_dir = cwd + '/pickles/'
     fin = ['bvert_11MeV.p', 'cpvert_11MeV.p', 'bvert_4MeV.p', 'cpvert_4MeV.p']
-    #fin = ['cpvert_4MeV.p']
     dets = [4, 5, 6 ,7 ,8 ,9, 10, 11, 12, 13, 14, 15]
 
-    for f in fin:
-        if '11' in f:
-            beam_11MeV = True
-        else:
-            beam_11MeV = False
-        if 'bvert' in f:
-            tilts = [0, 45, -45, 30, -30, 15, -15]
-        else:
-            tilts = [0, 30, -30, 15, -15]
+    # check individual tilt anlges
+    check_tilt = False
+    if check_tilt:
+        for f in fin:
+            if '11' in f:
+                beam_11MeV = True
+            else:
+                beam_11MeV = False
+            if 'bvert' in f:
+                tilts = [0, 45, -45, 30, -30, 15, -15]
+            else:
+                tilts = [0, 30, -30, 15, -15]
 
-        data = pd_load(f, p_dir)
-        data = split_filenames(data)
+            data = pd_load(f, p_dir)
+            data = split_filenames(data)
 
-        #tilt_check(data, dets, tilts, beam_11MeV)
-        map_3d(data, dets, tilts, beam_11MeV)
+            tilt_check(data, dets, tilts, beam_11MeV)
+    
+    # 3d plotting
+    data_bvert = pd_load(fin[0], p_dir)
+    data_bvert = split_filenames(data_bvert)
+    data_cpvert = pd_load(fin[1], p_dir)
+    data_cpvert = split_filenames(data_cpvert)
+
+    # crystal orientations ((a_x, a_y, a_z),(b_x, b_y, b_z),(c'_x, c'_y, c'_z))
+    b_up = np.asarray(((-1,0,0), (0,-1,0), (0,0,1)))
+    cp_up = np.asarray(((-1,0,0), (0,0,1), (0,-1,0)))
+
+    theta_n = [70, 60, 50, 40, 30, 20, 20, 30, 40, 50, 60, 70]
+    phi_n = [180, 180, 180, 180, 180, 180, 180, 0, 0, 0, 0, 0]
+    bvert_tilt = [0, 45, -45, 30, -30, 15, -15]
+    cpvert_tilt = [0, 30, -30, 15, -15]
+    beam_11MeV = True
+#    for d, det in enumerate(dets):       
+#        df_b_mapped = map_3d(data_bvert, det, bvert_tilt, b_up, theta_n[d], phi_n[d], beam_11MeV) 
+#        df_cp_mapped = map_3d(data_cpvert, det, cpvert_tilt, cp_up, theta_n[d], phi_n[d], beam_11MeV)
+#        plot_3d(df_b_mapped, df_cp_mapped, det, theta_n[d], beam_11MeV)
+#    plt.show()
+
+    data_bvert = pd_load(fin[2], p_dir)
+    data_bvert = split_filenames(data_bvert)
+    data_cpvert = pd_load(fin[3], p_dir)
+    data_cpvert = split_filenames(data_cpvert)
+    beam_11MeV = False
+    for d, det in enumerate(dets):       
+        df_b_mapped = map_3d(data_bvert, det, bvert_tilt, b_up, theta_n[d], phi_n[d], beam_11MeV=False) 
+        df_cp_mapped = map_3d(data_cpvert, det, cpvert_tilt, cp_up, theta_n[d], phi_n[d], beam_11MeV=False)
+        plot_3d(df_b_mapped, df_cp_mapped, det, theta_n[d], beam_11MeV)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
